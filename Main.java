e040012041d92b4a8f5ed0cc0533e25dab9d5ff1
Joshua Guimbaolibot <119920263+jumbo-hotdog@users.noreply.github.com>
2023-02-28 20:54:05 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Menu1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Difficulty difficulty = new Difficulty();
+
+    difficulty.setSize(465, 520);
+    difficulty.setVisible(true);
+    difficulty.setResizable(false);
+    difficulty.setLocationRelativeTo(null);
+    }
+}
